b12b1d1563fc10f2e0f98a1af8a73118f54b2129
syj <ssyyjj888@126.com>
2019-08-12 13:56:10 +0800

    新建工程
    Signed-off-by: syj <ssyyjj888@126.com>

---
+++ ThinkingSDK/ThinkingSDK.h
@@ -0,0 +1,19 @@
+//
+//  ThinkingSDK.h
+//  ThinkingSDK
+//
+//  Created by syj on 2019/8/12.
+//  Copyright © 2019 thinkingdata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThinkingSDK.
+FOUNDATION_EXPORT double ThinkingSDKVersionNumber;
+
+//! Project version string for ThinkingSDK.
+FOUNDATION_EXPORT const unsigned char ThinkingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThinkingSDK/PublicHeader.h>
+
+
