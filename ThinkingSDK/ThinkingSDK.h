c04866dc44a877d8e574a51cc9ce5313287825ea
syj <ssyyjj888@126.com>
2019-08-29 11:49:12 +0800

    no message

--- ThinkingSDK/ThinkingSDK.h
+++ ThinkingSDK/ThinkingSDK.h
@@ -1,13 +1,4 @@
 
-#import <UIKit/UIKit.h>
 #import <ThinkingSDK/ThinkingAnalyticsSDK.h>
-
-//! Project version number for ThinkingSDK.
-FOUNDATION_EXPORT double ThinkingSDKVersionNumber;
-
-//! Project version string for ThinkingSDK.
-FOUNDATION_EXPORT const unsigned char ThinkingSDKVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ThinkingSDK/PublicHeader.h>
 
 
---
+++ scripts/generates-documentation.sh
@@ -0,0 +1,9 @@
+jazzy   \
+ --objc   \
+ --author ThinkingData   \
+ --author_url http://http://www.thinkingdata.cn \
+ --module-version 2.1.0   \
+ --umbrella-header ThinkingSDK/Source/ThinkingAnalyticsSDK.h  \
+ --framework-root .   \
+ --module ThinkingSDK \
+ --sdk iphonesimulator \
