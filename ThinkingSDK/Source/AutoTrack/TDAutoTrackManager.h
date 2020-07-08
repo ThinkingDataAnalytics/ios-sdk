beb780a293fc26af029ffcf4b1fb16f9f563c960
Li,Huanan <329585368@qq.com>
2020-07-08 16:19:53 +0800

    [优化] - TDAutoTrackManager类优化, 现在单例不能使用init初始化了.

--- ThinkingSDK/Source/AutoTrack/TDAutoTrackManager.h
+++ ThinkingSDK/Source/AutoTrack/TDAutoTrackManager.h
@@ -1,22 +1,31 @@
 #import "ThinkingAnalyticsSDK.h"
 
-static NSString * const TD_EVENT_PROPERTY_TITLE = @"#title";
-static NSString * const TD_EVENT_PROPERTY_URL_PROPERTY = @"#url";
-static NSString * const TD_EVENT_PROPERTY_REFERRER_URL = @"#referrer";
-static NSString * const TD_EVENT_PROPERTY_SCREEN_NAME = @"#screen_name";
-static NSString * const TD_EVENT_PROPERTY_ELEMENT_ID = @"#element_id";
-static NSString * const TD_EVENT_PROPERTY_ELEMENT_TYPE = @"#element_type";
-static NSString * const TD_EVENT_PROPERTY_ELEMENT_CONTENT = @"#element_content";
-static NSString * const TD_EVENT_PROPERTY_ELEMENT_POSITION = @"#element_position";
+FOUNDATION_EXTERN NSString * const TD_EVENT_PROPERTY_TITLE;
+FOUNDATION_EXTERN NSString * const TD_EVENT_PROPERTY_URL_PROPERTY;
+FOUNDATION_EXTERN NSString * const TD_EVENT_PROPERTY_REFERRER_URL;
+FOUNDATION_EXTERN NSString * const TD_EVENT_PROPERTY_SCREEN_NAME;
+FOUNDATION_EXTERN NSString * const TD_EVENT_PROPERTY_ELEMENT_ID;
+FOUNDATION_EXTERN NSString * const TD_EVENT_PROPERTY_ELEMENT_TYPE;
+FOUNDATION_EXTERN NSString * const TD_EVENT_PROPERTY_ELEMENT_CONTENT;
+FOUNDATION_EXTERN NSString * const TD_EVENT_PROPERTY_ELEMENT_POSITION;
 
 @interface TDAutoTrackManager : NSObject
 
 + (instancetype)sharedManager;
+
 - (void)trackEventView:(UIView *)view;
+
 - (void)trackEventView:(UIView *)view withIndexPath:(NSIndexPath *)indexPath;
-- (void)viewControlWillAppear:(UIViewController *)controller;
+
 - (void)trackWithAppid:(NSString *)appid withOption:(ThinkingAnalyticsAutoTrackEventType)type;
+
+- (void)viewControlWillAppear:(UIViewController *)controller;
+
 + (UIViewController *)topPresentedViewController;
+
+#pragma mark - UNAVAILABLE
+- (instancetype)init NS_UNAVAILABLE;
++ (instancetype)new NS_UNAVAILABLE;
 
 @end
 
