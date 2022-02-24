2a5d5b347a55aee80d0a5d8a8727c8e2018dc3cb
wwango <wwango@thinkingdata.cn>
2022-02-24 14:42:25 +0800

    自动化测试脚本

--- Example/Pods/Target Support Files/Pods-ThinkingSDKDEMO/Pods-ThinkingSDKDEMO-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ThinkingSDKDEMO : NSObject
-@end
-@implementation PodsDummy_Pods_ThinkingSDKDEMO
-@end
--- ThinkingSDK/Source/Core/TDConfigurationMacros.h
+++
@@ -1,9 +0,0 @@
-//
-//  TDConfigurationMacros.h
-//  Pods
-//
-//  Created by wwango on 2022/1/11.
-//
-
-
-#define td_force_inline __inline__ __attribute__((always_inline))
