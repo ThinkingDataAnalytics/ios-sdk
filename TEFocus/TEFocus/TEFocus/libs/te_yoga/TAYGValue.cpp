e3d51926ba04ffda0428b4dcb7053c63a609d7aa
ThinkingData <sdk@thinkingdata.cn>
2023-08-31 19:25:33 +0800

    feat:Reorganize the file directory

--- TEFocus/TEFocus/TEFocus/libs/te_yoga/TAYGValue.cpp
+++
@@ -1,7 +0,0 @@
-
-
-#include "TAYGValue.h"
-
-const TAYGValue TAYGValueZero = {0, TAYGUnitPoint};
-const TAYGValue TAYGValueUndefined = {TAYGUndefined, TAYGUnitUndefined};
-const TAYGValue TAYGValueAuto = {TAYGUndefined, TAYGUnitAuto};
