acdb1ff175e9b477e54f9434cdfe37881840f97d
ThinkingData <sdk@thinkingdata.cn>
2024-04-28 15:47:32 +0800

    Release SDK

--- ThinkingSDK/Source/Hook/NSObject+TDDelegateProxy.h
+++ ThinkingSDK/Source/Hook/NSObject+TDDelegateProxy.h
@@ -15,15 +15,4 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
-@interface NSProxy (TDDelegateProxy)
-
-@property (nonatomic, copy, nullable) NSSet<NSString *> *thinkingdata_optionalSelectors;
-@property (nonatomic, strong, nullable) TDDelegateProxyObject *thinkingdata_delegateObject;
-
-/// hook respondsToSelector to resolve optional selectors
-/// @param aSelector selector
-- (BOOL)thinkingdata_respondsToSelector:(SEL)aSelector;
-
-@end
-
 NS_ASSUME_NONNULL_END
